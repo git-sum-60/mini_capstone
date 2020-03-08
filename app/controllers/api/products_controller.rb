32fe65ebb4584c2abe1d781b955c109c272cccd0
brian <riceb53@gmail.com>
2020-03-08 11:42:03 -0400

    add products show, create, update

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,32 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'index.json.jb'
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image_url: params[:image_url]
+    )
+
+    @product.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      description: params[:description] || @product.description,
+      image_url: params[:image_url] || @product.image_url
+    )
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/products" => "products#index"
+    get "/products/:id" => "products#show"
+    post "/products" => "products#create"
+    patch "/products/:id" => "products#update"
   end
 end
