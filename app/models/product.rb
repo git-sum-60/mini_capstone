d979bb49306b7f29b2297f95e62d8477051ec93c
brian <riceb53@gmail.com>
2020-03-15 13:29:57 -0400

    add has_many, belongs_to, also add to seeds file to associate prouducts that haven't been assigned a supplier (not as necessary to know as has_many belongs_to)

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,11 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :description, length: { in: 10..500 }
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
+  belongs_to :supplier
+
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
   
   def is_discounted?
     price < 10
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,5 +1,11 @@
 class Supplier < ApplicationRecord
-  def products
-    Product.where(supplier_id: id)
-  end
+  has_many :products
+
+  # must have table called products
+  # must have products table have a column called supplier_id
+  # column must be an integer
+  
+  # def products
+  #   Product.where(supplier_id: id)
+  # end
 end
