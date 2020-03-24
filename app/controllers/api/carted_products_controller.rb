fc14b37bc4ba510a762e2f3655c805a609166873
brian <riceb53@gmail.com>
2020-03-23 21:37:06 -0400

    add carted products index

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,4 +1,14 @@
 class Api::CartedProductsController < ApplicationController
+  
+  def index
+    # two filters
+      # only current_user
+      # only 'carted' status
+    # @carted_products = CartedProduct.where(user_id: current_user.id).where(status: 'carted')
+    @carted_products = current_user.carted_products.where(status: 'carted')
+    render 'index.json.jb'
+  end
+  
   def create
     @carted_product = CartedProduct.new(
       user_id: current_user.id,
@@ -10,4 +20,6 @@ class Api::CartedProductsController < ApplicationController
     @carted_product.save
     render 'show.json.jb'
   end
+
+
 end
